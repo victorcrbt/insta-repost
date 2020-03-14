54cb723c051d1c8e9ac064fe5c7ed15b4aa83300
Victor Batalha <madivcb@gmail.com>
2020-03-14 00:18:48 -0300

    eslint, prettier and stylelint setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,48 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  env: {
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:react-native-a11y/all',
+    'prettier',
+    'prettier/react',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'import', 'react-hooks', 'prettier', 'jsx-a11y'],
+  rules: {
+    'import/no-extraneous-dependencies': [
+      'error',
+      { devDependencies: ['**/Reactotron.js'] },
+    ],
+    'no-underscore-dangle': 'off',
+    'import/prefer-default-export': 'off',
+    'prettier/prettier': 'error',
+    'react/jsx-one-expression-per-line': 'off',
+    'no-param-reassign': 'off',
+    'global-require': 'off',
+    'react-native/no-raw-text': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    camelcase: 'off',
+    'no-console': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: ['.jsx', '.js'],
+      },
+    ],
+  },
 };
