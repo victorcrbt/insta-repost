aee4663290086ea522d9010d133f0f86536136d9
Victor Batalha <madivcb@gmail.com>
2020-03-14 00:53:06 -0300

    root-import setup

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React from 'react';
-import {SafeAreaView, Text, StatusBar} from 'react-native';
+import { SafeAreaView, Text, StatusBar } from 'react-native';
 
 const App = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView
-        style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
+      >
         <Text>Hello, World!</Text>
       </SafeAreaView>
     </>
