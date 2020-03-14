a7d3c24ebd889b365246daf80bf9b42baaa5478c
Victor Batalha <madivcb@gmail.com>
2020-03-14 15:26:57 -0300

    stack navigation header styles

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import { TouchableOpacity, Text } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+
+import openInstagram from '~/util/openInstagram';
 
 import Home from '~/screens/Home';
 
@@ -15,13 +19,23 @@ const screenOptions = {
   headerStyle: {
     backgroundColor: '#1579c1',
   },
+  headerRightContainerStyle: {
+    marginRight: 15,
+  },
+  headerRight: () => (
+    <TouchableOpacity onPress={openInstagram}>
+      <Text>
+        <Icon size={30} color="#fff" name="instagram" />
+      </Text>
+    </TouchableOpacity>
+  ),
 };
 
 const Routes = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={screenOptions}>
-        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Repost" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ src/util/openInstagram.js
@@ -0,0 +1,5 @@
+import { Linking } from 'react-native';
+
+export default () => {
+  return Linking.openURL('https://instagram.com');
+};
