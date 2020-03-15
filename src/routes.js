18992cb79924c0ff1c311506558d369d06bacc41
Victor Batalha <madivcb@gmail.com>
2020-03-14 22:39:46 -0300

    repost screen header styles

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { TouchableOpacity, Text } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import {
+  createStackNavigator,
+  CardStyleInterpolators,
+} from '@react-navigation/stack';
 
 import openInstagram from '~/util/openInstagram';
 
 import Home from '~/screens/Home';
+import Repost from '~/screens/Repost';
 
 const Stack = createStackNavigator();
 
@@ -22,20 +26,39 @@ const screenOptions = {
   headerRightContainerStyle: {
     marginRight: 15,
   },
-  headerRight: () => (
-    <TouchableOpacity onPress={openInstagram}>
-      <Text>
-        <Icon size={30} color="#fff" name="instagram" />
-      </Text>
-    </TouchableOpacity>
-  ),
 };
 
 const Routes = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={screenOptions}>
-        <Stack.Screen name="Repost" component={Home} />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            title: 'Repost',
+            headerRight: () => (
+              <TouchableOpacity onPress={openInstagram}>
+                <Text>
+                  <Icon size={30} color="#fff" name="instagram" />
+                </Text>
+              </TouchableOpacity>
+            ),
+          }}
+        />
+
+        <Stack.Screen
+          name="Repost"
+          component={Repost}
+          options={{
+            title: 'Repostar',
+            cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+            headerBackTitleVisible: false,
+            headerBackImage: () => (
+              <Icon name="chevron-left" size={30} color="#fff" />
+            ),
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
